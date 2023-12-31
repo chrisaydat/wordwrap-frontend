3ba7683898d691a250aa30005be0dfec491d9d28
chrisaydat <chrisaydat@gmail.com>
2023-12-31 22:00:59 +0000

    frontendchanges

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import logo from './wordwrapbyaydatverge.png'; // Import the logo
 
 function App() {
+  const [text, setText] = useState(""); // State to hold the input text
+
+  const handleInputChange = (event) => {
+    setText(event.target.value); // Update state with input value
+  };
+
+  const handleSubmit = () => {
+    alert(`Submitted Text: ${text}`); // Placeholder for submission action
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} alt="WordWrap Logo" className="App-logo" /> {/* Use the logo here */}
+        <textarea
+          value={text}
+          onChange={handleInputChange}
+          placeholder="Enter your text here"
+          className="text-input"
+        />
+        <button onClick={handleSubmit} className="submit-button">
+          Submit
+        </button>
       </header>
     </div>
   );
